e9126784e0bb3cccba292ad743a623aa1edaecc8
www <www3838438@yahoo.com>
2018-07-09 19:50:56 +0800

    init

--- get-git.sh
+++ get-git.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 #git -s --recurse-submodules
 
+
+git clone --recursive
 git submodule add https://github.com/cc3838438/vagrant-docker-compose.git source/vagrant-docker-compose
+
+git submodule foreach --recursive git checkout master
+git submodule foreach git pull
